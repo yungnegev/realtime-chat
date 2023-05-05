6a10a0d46713519133e48822f56bdc53835f81df
Arthur <neiklotrr@gmail.com>
2023-05-05 15:30:13 +0300

    Button component and it's corresponding utils

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,51 @@
-import { FC } from 'react'
+import { ButtonHTMLAttributes, FC } from 'react'
+import { VariantProps, cva } from 'class-variance-authority'
+import { Loader2 } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
-interface ButtonProps {}
+const buttonVariants = cva(
+    'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed',
+    {
+        variants: {
+            variant: {
+                default: 'bg-slate-900 text-white hover:bg-slate-800 ',
+                ghost: 'bg-transparent text-slate-900 hover:bg-slate-200',
+            },
+            size: {
+                default: 'h-10 px-4 py-2',
+                sm: 'h-9 px-2',
+                lg: 'h-11 px-8',
+            }
+        },
+        defaultVariants: {
+            variant: 'default',
+            size: 'default',
+        }
+    }
+)
 
-const Button: FC<ButtonProps> = ({}) => {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, 
+    VariantProps<typeof buttonVariants> {
+    isLoading?: boolean
+}
+
+const Button: FC<ButtonProps> = ({
+    className,
+    children,
+    variant,
+    isLoading,
+    size,
+    ...props
+}) => {
   return (
-    <div>
-        
-    </div>
+    <button
+        {...props}
+        className={cn(buttonVariants({ variant, size, className }))}
+        disabled={isLoading}
+        >
+        {isLoading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />}
+        {children}
+    </button>
   )
 }
 
