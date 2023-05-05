a5a760b1a284a0483cf1d4f120680a969626aaf3
Arthur <neiklotrr@gmail.com>
2023-05-05 18:29:58 +0300

    Auth options and their types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Button from "@/components/ui/Button"
+import { db } from "@/lib/db"
 
-const Home = () => {
+const Home = async () => {
+
   return (
     <div>
       <h1>Home</h1>
+
     </div>
   )
 }
